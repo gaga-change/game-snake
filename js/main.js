12d8a0faf62a05aaa829945f5ae8bec316583720
严俊东 <1172482914@qq.com>
2017-08-21 13:10:31 +0800

    '页面结构调整'

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
-init ()
-function createCanvas(w, h) {
+function createCanvas() {
+  var w = _w()
+  var h = _h()
   var element = document.createElement('canvas');
   element.setAttribute('id', 'demoCanvas')
-  element.setAttribute('width', w)
-  element.setAttribute('height', h)
+  element.setAttribute('width', String(w))
+  element.setAttribute('height', String(h))
   return element
 }
+
 function init() {
-  document.body.appendChild(createCanvas(500, 500))
+  // document.body.appendChild(createCanvas())
+  document.getElementById('app').appendChild(createCanvas())
   var stage = new createjs.Stage("demoCanvas");
   var circle = new createjs.Shape();
-  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50000);
   circle.x = 100;
   circle.y = 100;
   stage.addChild(circle);
@@ -22,4 +25,12 @@ function init() {
   createjs.Ticker.addEventListener("tick", function () {
     stage.update();
   });
+}
+
+function _h() {
+  return Math.floor(window.screen.height * 0.8)
+}
+
+function _w() {
+  return window.screen.width
 }
