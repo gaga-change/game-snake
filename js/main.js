21dc224a087c6dd20de059afd51e6fdc4a263c0d
严俊东 <1172482914@qq.com>
2017-08-21 15:57:02 +0800

    '格子完成'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+function init() {
+  document.getElementById('app').appendChild(createCanvas())
+  var stage = new createjs.Stage("demoCanvas");
+  var grid = new createjs.Shape();
+  // circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50000)
+  // circle.x = 100;
+  // circle.y = 100;
+  _drawGrid(grid.graphics)
+  stage.addChild(grid);
+  setTimeout(function () {
+    // circle.x = 200;
+    // console.log(circle)
+  }, 100)
+  createjs.Ticker.setFPS(60);
+  createjs.Ticker.addEventListener("tick", function () {
+    stage.update();
+  });
+}
+
+function _drawGrid(graphics) {
+  var gridW = Math.floor(_w() / 32)
+  var paddingW = _w() % gridW
+  var paddingH = _h() % gridW
+  var wNum = Math.floor(_w() / gridW)
+  var hNum = Math.floor(_h() / gridW)
+  console.log('paddingH', paddingH)
+  console.log('grid gridW', gridW)
+  var w = gridW
+  graphics.setStrokeStyle(1).beginStroke('#ffac52')
+  /* 横向 */
+  for (var i = 0; i <= hNum; i++) {
+    graphics.moveTo(paddingW / 2, i * w + paddingH / 2)
+    .lineTo(_w() - paddingW /2, i * w + paddingH / 2)
+  }
+  /* 纵向 */
+  for (i = 0; i <= wNum; i++) {
+    graphics.moveTo(i * w + paddingW / 2, paddingH / 2)
+    .lineTo(i * w + paddingW / 2, gridW * hNum + paddingH / 2)
+  }
+}
+
+/**
+ * 创建画布
+ * @returns {Element}
+ */
 function createCanvas() {
   var w = _w()
   var h = _h()
@@ -8,29 +53,20 @@ function createCanvas() {
   return element
 }
 
-function init() {
-  // document.body.appendChild(createCanvas())
-  document.getElementById('app').appendChild(createCanvas())
-  var stage = new createjs.Stage("demoCanvas");
-  var circle = new createjs.Shape();
-  circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50000);
-  circle.x = 100;
-  circle.y = 100;
-  stage.addChild(circle);
-  setTimeout(function () {
-    circle.x = 200;
-    console.log(circle)
-  }, 100)
-  createjs.Ticker.setFPS(60);
-  createjs.Ticker.addEventListener("tick", function () {
-    stage.update();
-  });
-}
-
+/**
+ * 获取canvas高度
+ * @returns {number}
+ * @private
+ */
 function _h() {
   return Math.floor(window.screen.height * 0.8)
 }
 
+/**
+ * 获取canvas宽度
+ * @returns {number}
+ * @private
+ */
 function _w() {
   return window.screen.width
 }
---
+++ test01.js
@@ -0,0 +1,5 @@
+/**
+ * Created by yanjd on 2017/8/21.
+ */
+module.exports = {b: 1}
+module.exports = {a: 1}
