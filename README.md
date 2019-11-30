c2b3efca413f91704838284178768ada34922830
严俊东 <1172482914@qq.com>
2019-11-30 16:52:46 +0800

    改版升级（重新梳理），添加了历史记录

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # html5贪吃蛇小游戏（PC端&移动端适配）
 
-> 主逻辑代码只有200行左右（有注释），Canvas绘制，绘制工具采用的是[EaselJS](https://github.com/CreateJS/EaselJS)。
+> 主逻辑代码只有300行左右（含注释），Canvas绘制，绘制工具采用的是[CreateJS](https://github.com/CreateJS/EaselJS)。
 
 demo地址：[https://snake.game.yanjd.top](https://snake.game.yanjd.top/)（若打不开或报错提[iesues](https://github.com/gaga-change/game-snake/issues)我会立即修复，或是[PR](https://github.com/gaga-change/game-snake/pulls)）
 
--- js/main.js
+++ js/main.js
@@ -1,53 +1,189 @@
-(function ($) {
-  /* 全局变量-四个方向 */
-  var UP = 1
-  var DOWN = -1
-  var LEFT = 2
-  var RIGHT = -2
-  /* 全局变量 - 游戏状态*/
-  var END = 1 // 结束
-  var READY = 2 // 准备
-  /* 蛇移动的方向 */
-  var DirectionOld = null
-  var DirectionNew = null
-  /* 游戏状态 */
-  var GameState = null
-  var Scope = null
+$(function () {
+  var LINE_WIDTH = 1 // 线条宽度
+  var LINE_MAX_NUM = 32 // 一行格子数量
+  var SNAKE_START_POINT = [[0, 3], [1, 3], [2, 3], [3, 3]] // 初始蛇坐标
+  var DIR_ENUM = { UP: 1, DOWN: -1, LEFT: 2, RIGHT: -2 }    // 移动的四个方向枚举值，两个对立方向相加等于0
+  var GAME_STATE_ENUM = { END: 1, READY: 2 } // 游戏状态枚举
+  var canvasHeight = $('canvas').height() // 获取canvas的高度
+  var canvasWidth = $('canvas').width() // 获取canvas的宽度
+  var gridWidth = (canvasWidth - LINE_WIDTH) / LINE_MAX_NUM // 格子宽度，按一行32个格子计算
+  var num = { w: LINE_MAX_NUM, h: Math.floor((canvasHeight - LINE_WIDTH) / gridWidth) } // 计算横向和纵向多少个格子，即：横坐标的最大值和纵坐标的最大值
+  var directionNow = null // 当前移动移动方向
+  var directionNext = null // 下一步移动方向
+  var gameState = null // 游戏状态
+  var scope = 0 // 分数
 
-  function init() {
-    updateScope(0) // 初始化分数
-    var snakes = _initSnake() // 初始化蛇的坐标
-    var fruits = []
-    fruits.push(createFruit(snakes, fruits)) // 初始化水果坐标，默认是两个
-    fruits.push(createFruit(snakes, fruits))
-    DirectionOld = DirectionNew = DOWN // 初始化蛇的移动方向
-    document.getElementById('content-canvas').innerHTML = ''
-    document.getElementById('content-canvas').appendChild(createCanvas()) // 创建画布
-    var stage = new createjs.Stage("demoCanvas")
-    var grid = new createjs.Shape()
-    var snake = new createjs.Shape()
-    var fruit = new createjs.Shape()
-    drawGrid(grid.graphics)
-    stage.addChild(grid)
-    stage.addChild(snake)
-    stage.addChild(fruit)
-    createjs.Ticker.setFPS(60)
-    createjs.Ticker.addEventListener("tick", function () {
-      stage.update()
-    })
-    GameState = READY // 准备流程结束
-    drawFruit(fruit.graphics, fruits)
-    move({ snake: snake.graphics, fruit: fruit.graphics }, snakes, fruits)
+  /**
+ * 绘制格子地图
+ * @param graphics
+ */
+  function drawGrid(graphics) {
+    var wNum = num.w
+    var hNum = num.h
+    graphics.setStrokeStyle(LINE_WIDTH).beginStroke('#ffac52')
+    // 画横向的线条
+    for (var i = 0; i <= hNum; i++) {
+      if (i === hNum || i === 0) graphics.setStrokeStyle(LINE_WIDTH)
+      if (i === 1) graphics.setStrokeStyle(0.1)
+      graphics.moveTo(LINE_WIDTH / 2, i * gridWidth + LINE_WIDTH / 2)
+        .lineTo(gridWidth * wNum + LINE_WIDTH / 2, i * gridWidth + LINE_WIDTH / 2)
+    }
+    graphics.setStrokeStyle(LINE_WIDTH)
+    // 画纵向的线条
+    for (i = 0; i <= wNum; i++) {
+      if (i === wNum || i === 0) graphics.setStrokeStyle(LINE_WIDTH)
+      if (i === 1) graphics.setStrokeStyle(.1)
+      graphics.moveTo(i * gridWidth + LINE_WIDTH / 2, LINE_WIDTH / 2)
+        .lineTo(i * gridWidth + LINE_WIDTH / 2, gridWidth * hNum + LINE_WIDTH / 2)
+    }
+  }
+
+  /** 
+   * 坐标类
+   */
+  function Point(x, y) {
+    this.x = x
+    this.y = y
   }
 
   /**
-   * 绑定相关事件（方向键、开始按钮、结束按钮的点击）
+   * 根据移动的方向，获取当前坐标的下一个坐标
+   * @param direction 移动的方向
    */
-  function initEvent() {
-    $('#UpBtn').click(function () { changeDirection(UP) })
-    $('#LeftBtn').click(function () { changeDirection(LEFT) })
-    $('#RightBtn').click(function () { changeDirection(RIGHT) })
-    $('#DownBtn').click(function () { changeDirection(DOWN) })
+  Point.prototype.nextPoint = function nextPoint(direction) {
+    var point = new Point(this.x, this.y)
+    switch (direction) {
+      case DIR_ENUM.UP:
+        point.y -= 1
+        break
+      case DIR_ENUM.DOWN:
+        point.y += 1
+        break
+      case DIR_ENUM.LEFT:
+        point.x -= 1
+        break
+      case DIR_ENUM.RIGHT:
+        point.x += 1
+        break
+    }
+    return point
+  }
+
+  /**
+ * 初始化蛇的坐标
+ * @returns {[Point,Point,Point,Point,Point ...]}
+ * @private
+ */
+  function initSnake() {
+    return SNAKE_START_POINT.map(function (item) {
+      return new Point(item[0], item[1])
+    })
+  }
+
+  /**
+   * 绘制蛇
+   * @param graphics
+   * @param snakes // 蛇坐标
+   */
+  function drawSnake(graphics, snakes) {
+    graphics.clear()
+    graphics.beginFill("#a088ff")
+    var len = snakes.length
+    for (var i = 0; i < len; i++) {
+      if (i === len - 1) graphics.beginFill("#ff6ff9")
+      graphics.drawRect(
+        snakes[i].x * gridWidth + LINE_WIDTH / 2,
+        snakes[i].y * gridWidth + LINE_WIDTH / 2,
+        gridWidth, gridWidth)
+    }
+  }
+
+  /**
+ * 改变蛇身坐标
+ * @param snakes 蛇坐标集
+ * @param direction 方向
+ */
+  function updateSnake(snakes, fruits, direction, fruitGraphics) {
+    var oldHead = snakes[snakes.length - 1]
+    var newHead = oldHead.nextPoint(direction)
+    // 超出边界 游戏结束
+    if (newHead.x < 0 || newHead.x >= num.w || newHead.y < 0 || newHead.y >= num.h) {
+      gameState = GAME_STATE_ENUM.END
+    } else if (snakes.some(function (p) { // ‘吃’到自己 游戏结束
+      return newHead.x === p.x && newHead.y === p.y
+    })) {
+      gameState = GAME_STATE_ENUM.END
+    } else if (fruits.some(function (p) { // ‘吃’到水果
+      return newHead.x === p.x && newHead.y === p.y
+    })) {
+      updateScope()
+      snakes.push(newHead)
+      var temp = 0
+      fruits.forEach(function (p, i) {
+        if (newHead.x === p.x && newHead.y === p.y) {
+          temp = i
+        }
+      })
+      fruits.splice(temp, 1)
+      var newFruit = createFruit(snakes, fruits)
+      if (newFruit) {
+        fruits.push(newFruit)
+        drawFruit(fruitGraphics, fruits)
+      }
+    } else {
+      snakes.push(newHead)
+      snakes.shift()
+    }
+  }
+
+  /**
+   * 引擎
+   * @param graphics
+   * @param snakes
+   */
+  function start(snakeGraphics, fruitGraphics, snakes, fruits, stage) {
+    clearTimeout(window._engine) // 重启时关停之前的引擎
+    run()
+    function run() {
+      directionNow = directionNext
+      updateSnake(snakes, fruits, directionNow, fruitGraphics) // 更新蛇坐标
+      if (gameState === GAME_STATE_ENUM.END) {
+        end()
+      } else {
+        drawSnake(snakeGraphics, snakes)
+        stage.update()
+        window._engine = setTimeout(run, 500 * Math.pow(0.9, scope))
+      }
+    }
+  }
+
+  /**
+   * 游戏结束回调
+   */
+  function end() {
+    gameState = GAME_STATE_ENUM.END
+    renderHistory(scope)
+    $('#MenuArea').show()
+  }
+
+  /**
+   * 改变蛇行进方向
+   * @param dir
+   */
+  function changeDirection(dir) {
+    /* 逆向及同向则不改变 */
+    if (directionNow + dir === 0 || directionNow === dir) return
+    directionNext = dir
+  }
+
+  /**
+   * 绑定相关元素点击事件
+   */
+  function bindEvent() {
+    $('#UpBtn').click(function () { changeDirection(DIR_ENUM.UP) })
+    $('#LeftBtn').click(function () { changeDirection(DIR_ENUM.LEFT) })
+    $('#RightBtn').click(function () { changeDirection(DIR_ENUM.RIGHT) })
+    $('#DownBtn').click(function () { changeDirection(DIR_ENUM.DOWN) })
     $('#EndBtn').click(end)
     $('#StartBtn').click(function () {
       $('#MenuArea').hide()
@@ -72,130 +208,67 @@
     $(window).keydown(function (e) {
       switch (e.keyCode) {
         case 37:
-          changeDirection(LEFT)
+          changeDirection(DIR_ENUM.LEFT)
           $('#LeftBtn').eq(0).focus()
           break
         case 38:
-          changeDirection(UP)
+          changeDirection(DIR_ENUM.UP)
           $('#UpBtn').eq(0).focus()
           break
         case 39:
-          changeDirection(RIGHT)
+          changeDirection(DIR_ENUM.RIGHT)
           $('#RightBtn').eq(0).focus()
           break
         case 40:
-          changeDirection(DOWN)
+          changeDirection(DIR_ENUM.DOWN)
           $('#DownBtn').eq(0).focus()
           break
       }
     })
   }
 
-  /** 
-   * 游戏结束dom处理
-  */
-  function end() {
-    GameState = END
-    $('#MenuArea').show()
-  }
-
   /**
-   * 引擎
-   * @param graphics
-   * @param snakes
-   * @param fruits
-   */
-  function move(graphics, snakes, fruits) {
-    clearTimeout(window.engine) // 重启时关停之前的引擎
-    _run()
-
-    function _run() {
-      DirectionOld = DirectionNew
-      fruits = updateSnake(snakes, DirectionOld, fruits) || fruits
-      if (GameState === END) {
-        end()
-        return
+ * 创建水果坐标
+ * @returns Point
+ * @param snakes
+ * @param fruits
+ */
+  function createFruit(snakes, fruits) {
+    var totals = {}
+    for (var x = 0; x < num.w; x++) {
+      for (var y = 0; y < num.h; y++) {
+        totals[x + '-' + y] = true
       }
-      if (fruits.length < 2) {
-        fruits.push(createFruit(snakes, fruits))
-        drawFruit(graphics.fruit, fruits)
-      }
-      drawSnake(graphics.snake, snakes)
-      window.engine = setTimeout(_run, 500 * Math.pow(0.9, Scope))
+    }
+    snakes.forEach(function (item) {
+      delete totals[item.x + '-' + item.y]
+    })
+    fruits.forEach(function (item) {
+      delete totals[item.x + '-' + item.y]
+    })
+    var keys = Object.keys(totals)
+    if (keys.length) {
+      var temp = Math.floor(keys.length * Math.random())
+      var key = keys[temp].split('-')
+      return new Point(Number(key[0]), Number(key[1]))
+    } else {
+      return null
     }
   }
 
   /**
-   * 绘制水果
-   * @param graphics
-   * @param fruits 水果坐标集
-   */
+ * 绘制水果
+ * @param graphics
+ * @param fruits 水果坐标集
+ */
   function drawFruit(graphics, fruits) {
     graphics.clear()
     graphics.beginFill("#16ff16")
     for (var i = 0; i < fruits.length; i++) {
       graphics.drawRect(
-        fruits[i].x * _gridW() + _padding().w / 2,
-        fruits[i].y * _gridW() + _padding().h / 2,
-        _gridW(), _gridW())
-    }
-  }
-
-  /**
-   * 创建水果坐标
-   * @returns Point
-   * @param snakes
-   * @param fruits
-   */
-  function createFruit(snakes, fruits) {
-    return (function _createPoint() {
-      var p = new Point(Math.floor(Math.random() * _num().w), Math.floor(Math.random() * _num().h))
-      if (snakes.some(function (point) {
-        return p.x === point.x && p.y === point.y
-      }) || fruits.some(function (point) {
-        return p.x === point.x && p.y === point.y
-      })) {
-        return _createPoint()
-      }
-      return p
-    })()
-  }
-
-  /**
-   * 改变蛇身坐标
-   * @param snakes 蛇坐标集
-   * @param direction 方向
-   * @param fruits 水果坐标集
-   */
-  function updateSnake(snakes, direction, fruits) {
-    var oldHead = snakes[snakes.length - 1]
-    var p = new Point(oldHead.x, oldHead.y)
-    p.update(_gridW(), direction)
-    // 超出边界 游戏结束
-    if (p.x < 0 || p.x >= _num().w || p.y < 0 || p.y >= _num().h) {
-      GameState = END
-      return
-    }
-    fruits = fruits.filter(function (point) {
-      return !(point.x === p.x && point.y === p.y)
-    })
-    var shiftPoint = null
-    if (fruits.length === 2) // 没有吃到水果才消除尾巴
-      shiftPoint = snakes.shift()
-    snakes.push(p)
-    // ‘吃’到自己 游戏结束
-    if (snakes.some(function (p1, index1) {
-      return snakes.some(function (p2, index2) {
-        return p2.x === p1.x && p2.y === p1.y && index1 !== index2
-      })
-    })) {
-      if (fruits.length === 2) snakes.unshift(shiftPoint) // 还原被删掉的末尾
-      snakes.pop() // 删除新添的头部
-      GameState = END
-    }
-    if (fruits.length < 2) { // 如果水果没有减少则不更新现有的水果坐标数组
-      updateScope()
-      return fruits
+        fruits[i].x * gridWidth + LINE_WIDTH / 2,
+        fruits[i].y * gridWidth + LINE_WIDTH / 2,
+        gridWidth, gridWidth)
     }
   }
 
@@ -203,169 +276,61 @@
    * 更新页面中显示的分数
    */
   function updateScope(num) {
-    if (num !== undefined) Scope = 0
+    if (num !== undefined) scope = num
     else
-      Scope++
-    $('#scope').text(Scope)
+      scope++
+    $('#scope').text(scope)
   }
 
-  /**
-   * 绘制蛇
-   * @param graphics
-   * @param snakes // 蛇坐标
-   */
-  function drawSnake(graphics, snakes) {
-    graphics.clear()
-    graphics.beginFill("#a088ff")
-    for (var i = 0; i < snakes.length; i++) {
-      if (i === snakes.length - 1) graphics.beginFill("#ff6ff9")
-      graphics.drawRect(
-        snakes[i].x * _gridW() + _padding().w / 2,
-        snakes[i].y * _gridW() + _padding().h / 2,
-        _gridW(), _gridW())
+  /** 更新历史记录 */
+  function renderHistory(scope) {
+    var history = localStorage.getItem('HISTORY')
+    try {
+      history = JSON.parse(history)
+    } catch (err) {
+      history = []
+    }
+    history = history || []
+    if (scope) {
+      history.push(scope)
+      history.sort(function (a, b) {
+        return b - a
+      })
+      history.splice(3, 1)
+      window.localStorage.setItem('HISTORY', JSON.stringify(history))
+    }
+    $('#HistoryArea').empty()
+    history.forEach(function (scope, index) {
+      $('#HistoryArea').append('<p>' + (index + 1) + '：' + scope + '分</p>')
+    })
+    if (!history.length) {
+      $('#HistoryArea').append('<p>暂无记录</p>')
     }
   }
 
-  /**
-   * 改变蛇行进方向
-   * @param dir
-   */
-  function changeDirection(dir) {
-    /* 逆向及同向则不改变 */
-    if (DirectionOld + dir === 0
-      || DirectionOld === dir
-    ) return
-    DirectionNew = dir
+  function init() {
+    $('canvas').attr('width', canvasWidth) // 给canvas设置宽高属性赋值上当前canvas的宽度和高度（单用样式配置宽高会被拉伸）
+    $('canvas').attr('height', canvasHeight)
+    directionNow = directionNext = DIR_ENUM.DOWN // 初始化蛇的移动方向
+    gameState = GAME_STATE_ENUM.READY
+    updateScope(0)
+    var snakes = initSnake()
+    var fruits = []
+    fruits.push(createFruit(snakes, fruits))
+    fruits.push(createFruit(snakes, fruits))
+    var stage = new createjs.Stage($('canvas')[0])
+    var grid = new createjs.Shape()
+    var snake = new createjs.Shape()
+    var fruit = new createjs.Shape()
+    drawGrid(grid.graphics) // 绘制格子
+    drawSnake(snake.graphics, snakes)
+    drawFruit(fruit.graphics, fruits)
+    stage.addChild(grid)
+    stage.addChild(snake)
+    stage.addChild(fruit)
+    stage.update()
+    start(snake.graphics, fruit.graphics, snakes, fruits, stage)
   }
-
-  /**
-   * 绘制格子地图
-   * @param graphics
-   */
-  function drawGrid(graphics) {
-    var gridW = _gridW()
-    var paddingW = _padding().w
-    var paddingH = _padding().h
-    var wNum = _num().w
-    var hNum = _num().h
-    var w = gridW
-    graphics.setStrokeStyle(.1).beginStroke('#ffac52')
-    /* 横向 */
-    for (var i = 0; i <= hNum; i++) {
-      if (i === hNum || i === 0) graphics.setStrokeStyle(1)
-      if (i === 1) graphics.setStrokeStyle(.1)
-      graphics.moveTo(paddingW / 2, i * w + paddingH / 2)
-        .lineTo(_w() - paddingW / 2, i * w + paddingH / 2)
-    }
-    graphics.setStrokeStyle(.1)
-    /* 纵向 */
-    for (i = 0; i <= wNum; i++) {
-      if (i === wNum || i === 0) graphics.setStrokeStyle(1)
-      if (i === 1) graphics.setStrokeStyle(.1)
-      graphics.moveTo(i * w + paddingW / 2, paddingH / 2)
-        .lineTo(i * w + paddingW / 2, gridW * hNum + paddingH / 2)
-    }
-  }
-
-  /**
-   * 创建画布
-   * @returns {Element}
-   */
-  function createCanvas() {
-    var w = _w()
-    var h = _h()
-    var element = document.createElement('canvas')
-    element.setAttribute('id', 'demoCanvas')
-    element.setAttribute('width', String(w))
-    element.setAttribute('height', String(h))
-    return element
-  }
-
-  /**
-   * 初始化蛇的坐标
-   * @returns {[Point,Point,Point,Point,Point ...]}
-   * @private
-   */
-  function _initSnake() {
-    return [
-      new Point(0, 0),
-      new Point(1, 0),
-      new Point(2, 0),
-      new Point(3, 0)
-    ]
-  }
-
-  /**
-   * 获取格子宽度（正方形）
-   * @returns {number}
-   * @private
-   */
-  function _gridW() {
-    return Math.floor(_w() / 32)
-  }
-
-  /**
-   * 获取横向和纵向的格子数量
-   * @returns {{w: number, h: number}}
-   * @private
-   */
-  function _num() {
-    return {
-      w: Math.floor(_w() / _gridW()),
-      h: Math.floor(_h() / _gridW())
-    }
-  }
-
-  /**
-   * 获取绘制地图补丁（如宽11，格子宽2，则补丁是1，因为没有二分之一的格子）
-   * @returns {{w: number, h: number}}
-   * @private
-   */
-  function _padding() {
-    return {
-      w: _w() % _gridW(),
-      h: _h() % _gridW()
-    }
-  }
-
-  /**
-   * 获取canvas高度
-   * @returns {number}
-   * @private
-   */
-  function _h() {
-    return Math.floor(document.body.scrollHeight * 0.8)
-  }
-
-  /**
-   * 获取canvas宽度
-   * @returns {number}
-   * @private
-   */
-  function _w() {
-    return Math.min(document.body.scrollWidth, 768)
-  }
-
-  function Point(x, y) {
-    this.x = x
-    this.y = y
-    this.update = function (gridW, direction) {
-      switch (direction) {
-        case UP:
-          this.y -= 1
-          break
-        case DOWN:
-          this.y += 1
-          break
-        case LEFT:
-          this.x -= 1
-          break
-        case RIGHT:
-          this.x += 1
-          break
-      }
-    }
-  }
-
-  initEvent() // 绑定事件
-}($))
+  renderHistory()
+  bindEvent()
+})
